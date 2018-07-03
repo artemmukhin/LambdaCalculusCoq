a3d5ee1914bf699a784a141b2e5888cc1effc0af
ortem <ortem00@gmail.com>
2018-07-03 13:24:19 +0300

    Add height

--- Term.v
+++ Term.v
@@ -6,9 +6,26 @@ Inductive term : Type :=
     | App : term -> term -> term
     | Lam : id -> term -> term.
 
+Hint Constructors term.
+
 Definition x := (Id 1).
 Definition y := (Id 2).
 Definition z := (Id 3).
+
 Hint Unfold x.
 Hint Unfold y.
 Hint Unfold z.
+
+Fixpoint height t :=
+  match t with
+  | Var _   => 0
+  | App m n => 1 + max (height m) (height n)
+  | Lam _ m => 1 + height m
+  end.
+
+Lemma induction_by_height : 
+   forall P : term -> Prop, 
+   (forall t, height t = 0 => P t) -> 
+   (forall n t, height t = n -> P t => (forall t', height t' = n+1 -> P t')) ->
+   (forall t, P t).
+
